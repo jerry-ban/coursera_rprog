1825503e63dda83aba97985d98feb1a75146b6a2
jerry-ban <jerry.ban@analyticpartners.com>
2018-01-19 18:00:08 -0500

    hw2 test

---
+++ R-datetime.R
@@ -0,0 +1,3 @@
+#date is reped by Date class, time is represented by POSIXct or the POSIXlt class
+# dates are stored internally as # of days since 1970-01-01
+# time are stored internally as # of seconds since 1970-01-01
---
+++ hw2_2.R
@@ -0,0 +1,42 @@
+#  if else
+if(TRUE){
+    ##
+} else if(FALSE){
+    ##
+} else{
+    ##
+}
+x<-5
+y<-if(x>3){TRUE} else {FALSE}
+
+y
+#  for
+x<-c("a","b","c","d")
+for(i in 1:4) {
+    print(x[i])
+}
+for(i in 1:4) print(x[i])
+
+seq_len((10))
+seq_along(2:10)
+for (i in seq_along(x)){
+    print(x[i])
+}
+for (letter in x){
+    print(letter)
+}
+#  while
+#  repeat : execute an inlinite loop
+#   break : break a loop
+#   next : skip the  iteration of a loop
+#   return: exit a function
+set.seed(1)
+normals<-rnorm(100,1,2)
+normals
+summary(normals)
+sd(normals)
+
+x<-seq(1.7,1.9, len=11)
+y<- sapply(x, function(y){y*y})
+y
+plot(x, exp(-(y-min(y))),type = "l")
